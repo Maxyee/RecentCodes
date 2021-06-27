1b9a8748c9498046767d78d5b9636f9f2d00861a
maxyee <eyaminkhan00@gmail.com>
2021-06-27 16:43:40 +0600

    implemented the socketio and enduser function

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
+const io = require('socket.io')(server)
 const { v4: uuidv4 } = require('uuid');
 
 app.set('view engine', 'ejs');
@@ -16,6 +17,14 @@ app.get('/:room', (req, res) => {
     res.render('room', { roomId: req.params.room });
 })
 
+io.on('connection', socket => {
+    socket.on('join-room', (roomId) => {
+        socket.join(roomId);
+        //socket.to(roomId).broadcast.emit('user-connected');
+        socket.broadcast.emit('user-connected', roomId);
+        //console.log('joined Room')
+    })
+})
 
 
 server.listen(3030);
